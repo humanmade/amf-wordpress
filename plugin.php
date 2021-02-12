d4948389ebf713b63f70bf66d1f51bd9802a71bc
Thorsten Frommen <info@tfrommen.de>
2021-02-12 11:23:22 +0100

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -27,6 +27,4 @@ if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
 	include_once __DIR__ . '/vendor/autoload.php';
 }
 
-include_once __DIR__ . '/wp-content/plugins/asset-manager-framework/plugin.php';
-
 bootstrap();
